1c7ae56499218b594f74f866eb787e0c2e8a326e
Zhihe <cubric@Zhihes-MBP.localdomain>
2019-04-04 09:43:30 +0800

    Add random user string and random proxies

---
+++ fang/pipelines.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import psycopg2
+
+class FangPipeline(object):
+    def __init__(self):
+        hostname = 'localhost'
+        username = "cubric"
+        database = "ke"
+        self.connection = psycopg2.connect(host=hostname, user=username, dbname=database)
+
+    def process_item(self, item, spider):
+        self.cur = self.connection.cursor()
+        self.cur.execute("""
+            insert into raw_data(city, name, status, locations,
+                room, tags, avg_price, total_price) values(%s, %s, %s, %s, %s, %s, %s, %s);""",(
+                    item.get('city', 'NA'),
+                    item.get('name', 'NA'),
+                    item.get('status', 'NA'),
+                    item.get('locations', 'NA'),
+                    item.get('room', 'NA'),
+                    item.get('tags', 'NA'),
+                    item.get('avg_price', 'NA'),
+                    item.get('total_price', 'NA')
+                    )
+                )
+        self.connection.commit()
+        return item
--- fang/settings.py
+++ fang/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for ke project
+# Scrapy settings for fang project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,17 +9,16 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'ke'
+BOT_NAME = 'fang'
 
-SPIDER_MODULES = ['ke.spiders']
-NEWSPIDER_MODULE = 'ke.spiders'
-
+SPIDER_MODULES = ['fang.spiders']
+NEWSPIDER_MODULE = 'fang.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0'
+#USER_AGENT = 'fang (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+#ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +26,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+#DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -47,40 +46,34 @@ DOWNLOAD_DELAY = 5
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'ke.middlewares.KeSpiderMiddleware': 543,
+#    'fang.middlewares.FangSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'ke.middlewares.KeDownloaderMiddleware': 543,
+#    'fang.middlewares.FangDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#    'scrapy.extensions.closespider.CloseSpider':500
 #}
-#CLOSESPIDER_PAGECOUNT=90
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'ke.pipelines.KePipeline': 300,
+    'fang.pipelines.FangPipeline': 300,
 }
-MONGODB_SERVER = "localhost"
-MONGODB_PORT = 27017
-MONGODB_DB = "housing"
-MONGODB_COLLECTION = "loupan"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+#AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+#AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 25
+#AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
@@ -94,3 +87,57 @@ AUTOTHROTTLE_MAX_DELAY = 25
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 32
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+    # For retries
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 290,
+    # For random scylla proxies
+    'scrapy_scylla_proxies.random_proxy.RandomProxyMiddleware': 300,
+    # For http proxy ip rotation
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 750,
+}
+
+DOWNLOAD_TIMEOUT = 180
+RETRY_TIMES = 10
+RETRY_HTTP_CODES = [500, 502, 503, 504, 408, 400, 429, 403, 404]
+
+# scrapy-scylla-proxies settings
+# Enabled
+SSP_ENABLED = True
+# Location of the scylla server
+SSP_SCYLLA_URI = 'http://localhost:8899'
+# Proxy timeout in seconds
+SSP_PROXY_TIMEOUT = 60
+# Get only https proxies
+SSP_HTTPS = True
+
+
+# -----------------------------------------------------------------------------
+# USER AGENT
+# -----------------------------------------------------------------------------
+
+DOWNLOADER_MIDDLEWARES.update({
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+})
+
+USER_AGENTS = [
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/57.0.2987.110 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.79 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+     'Gecko/20100101 '
+     'Firefox/55.0')  # firefox
+]
+
+
